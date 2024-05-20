2d86b2272e70c71b56d3e4df0ce2e7678300a5f8
Alex Vargas <alexv7142@gmail.com>
2024-05-19 22:09:26 -0600

    Proof for new branch

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CoreModule } from './core/core.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://admin:1234@localhost:27017/boardlifyDB?authSource=admin'),
     UserModule,
+    CoreModule,
   ],
   controllers: [],
   providers: [],
